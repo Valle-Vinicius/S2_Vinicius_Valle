7575fa9f6bc42e5ab183d21548f3e250b21761eb
Valle-Vinicius <vinicius.v.rodrigues6@aluno.senai.br>
2025-04-08 08:42:34 -0300

    Criação da Branch Incluir1
    Criação da Branch "Incluir1", juntamente com a alteração do arquivo "Saudação.py"

--- saudacao.py
+++ saudacao.py
@@ -2,4 +2,18 @@ def saudacao():
     nome = input("Qual o seu nome? ")
     print(f"Olá, {nome}!")
 
-saudacao()
+def escolha_opcao():
+    print("Escolha uma opção:")
+    print("1. Saudação")
+    print("2. Sair")
+    opcao = input("Digite sua opção: ")
+    
+    if opcao == '1':
+        saudacao()
+    elif opcao == '2':
+        print("Saindo...")
+    else:
+        print("Opção inválida.")
+        escolha_opcao()  
+
+escolha_opcao()
